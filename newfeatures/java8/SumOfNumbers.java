6be3c9a933cdb1629c45d4daddfbdd994b4a2b61
singhsb-git <34877107+singhsb-git@users.noreply.github.com>
2019-11-07 14:49:09 -0500

    Update SumOfNumbers.java

--- newfeatures/java8/SumOfNumbers.java
+++ newfeatures/java8/SumOfNumbers.java
@@ -10,18 +10,26 @@ public class TestClass {
 
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5);
 
-        // sum list of numbers
+        // sum of list of numbers
         Integer sum = integers.stream()
                 .collect(Collectors.summingInt(Integer::intValue));
         System.out.println("Sum ------->"+ sum);
 
-        // sum list of square of numbers
+        // sum of list of square of numbers
         Integer sumSq = integers.stream()
                 .map(val -> getSquare(val))
                 .collect(Collectors.summingInt(Integer::intValue));
-        System.out.println("Sum of squares ------->"+ sumSq);
+        System.out.println("Sum of squares using map & summingInt ------->"+ sumSq);
 
+        // sum of list of numbers reduce
+        Integer sum1 = integers.stream()
+                .reduce(0, (a, b) -> a + b);
+        System.out.println("Sum using reduce ------->"+ sum1);
 
+        // sum of list of numbers using reduce (Integer::sum)
+        Integer sum2 = integers.stream()
+                .reduce(0, Integer::sum);
+        System.out.println("Sum using reduce (Integer::sum)------->"+ sum2);
     }
 
     public static int getSquare(int val){
