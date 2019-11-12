304a40e2559ce6cf6bb84afbb0d9eb657daa6e95
singhsb-git <34877107+singhsb-git@users.noreply.github.com>
2019-11-12 18:05:51 -0500

    Update SumOfNumbers.java

--- newfeatures/java8/SumOfNumbers.java
+++ newfeatures/java8/SumOfNumbers.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TestClass {
+public class SumOfNumbers {
 
     public static void main(String args[]){
 
